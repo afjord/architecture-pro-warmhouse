534d3ae02feea43fed7ea900b72e296118289065
Andrei Fyodorov <afjord@ya.ru>
2026-01-13 01:20:07 +0300

    Complete task 5.

--- apps/temperature-api/app/src/main/java/org/example/App.java
+++ apps/temperature-api/app/src/main/java/org/example/App.java
@@ -2,11 +2,23 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class App {
 
     static void main(String... args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(false);
+        filter.setIncludeHeaders(false);
+        filter.setIncludeClientInfo(true);
+        return filter;
     }
 }
--- apps/temperature-api/app/src/main/java/org/example/TemperatureController.java
+++ apps/temperature-api/app/src/main/java/org/example/TemperatureController.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +36,16 @@ public class TemperatureController {
             };
         }
         return TemperatureData.getRandom(location, sensorId);
+    }
+
+    @GetMapping("temperature/{id}")
+    public TemperatureData getTemperatureBySensorId(@PathVariable String id) {
+        String location = switch (id) {
+            case "1" -> "Living Room";
+            case "2" -> "Bedroom";
+            case "3" -> "Kitchen";
+            default -> "Unknown";
+        };
+        return TemperatureData.getRandom(location, id);
     }
 }
