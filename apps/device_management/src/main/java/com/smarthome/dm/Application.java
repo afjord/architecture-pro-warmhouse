b391978d053b859fc277e089dc2a9f141546d95a
Andrei Fyodorov <afjord@ya.ru>
2026-01-16 23:09:03 +0300

    Fix Docker for device management

--- apps/device_management/src/main/java/com/smarthome/dm/Application.java
+++ apps/device_management/src/main/java/com/smarthome/dm/Application.java
@@ -3,6 +3,8 @@ package com.smarthome.dm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -10,5 +12,15 @@ public class Application {
 
     static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(false);
+        filter.setIncludeHeaders(false);
+        filter.setIncludeClientInfo(true);
+        return filter;
     }
 }
